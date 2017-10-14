e522e2126d95817737623c02fd4a998177e220b8
Yvesis <hdisrael7@gmail.com>
2017-10-14 00:12:33 -0400

    3.4 shopping cart done!

--- TP3/assets/scripts/controllers/products-controller.js
+++ TP3/assets/scripts/controllers/products-controller.js
@@ -9,7 +9,6 @@
       items = data;
     });
     $("#products-list a>img").click((evt) => {
-      evt.preventDefault();
       let a =$(evt.target);
       let h2 =$(a).prev("h2");
       localStorage.setItem("selected-product",items.filter(item => item.name === $(h2[0]).text())[0].id );
