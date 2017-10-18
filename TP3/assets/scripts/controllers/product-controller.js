31036b44acda8f351ca90456b060eb747402c6b9
oualidmaz <mazouzioualid@gmail.com>
2017-10-18 16:15:28 -0400

    3.3.2 draft

--- TP3/assets/scripts/controllers/product-controller.js
+++ TP3/assets/scripts/controllers/product-controller.js
@@ -9,13 +9,13 @@
       return false;
     });
     // hide shopping cart if there is no item on it
-    var nbrItemOnShoppingCart = JSON.parse(localStorage.getItem("shopping-cart")).length;
-    $('.shopping-cart .count').html(nbrItemOnShoppingCart);
-    if(nbrItemOnShoppingCart === 0){
-      $('.shopping-cart .count').hide();
-    }else{
-      $('.shopping-cart .count').show();
-    }
+    // var nbrItemOnShoppingCart = JSON.parse(localStorage.getItem("shopping-cart")).length;
+    // $('.shopping-cart .count').html(nbrItemOnShoppingCart);
+    // if(nbrItemOnShoppingCart === 0){
+    //   $('.shopping-cart .count').hide();
+    // }else{
+    //   $('.shopping-cart .count').show();
+    // }
   });
 
 })();
--- TP3/assets/scripts/controllers/products-controller.js
+++ TP3/assets/scripts/controllers/products-controller.js
@@ -16,19 +16,65 @@
     });
 
       listProduct(items);
+      updateNbrProducts(items);
     });
 
     function listProduct(products){
       $('#products-list').html('');
       $.each(products, function(index, item) {
         $('#products-list').append(`
-        <a href="./product.html" title="En savoir plus...">
+        <a href="./product.htmll?id=#${item.id}" title="En savoir plus...">
           <h2>${item.name}</h2>
           <img alt="${item.name}" src="./assets/img/${item.image}">
           <p><small>Prix</small> ${item.price}&thinsp;$</p>
         </a>
         `);
      });
+    }
+
+    function sortByPriceASC(a,b){
+       return a.price - b.price;
+    }
+
+    function sortByPriceDESC(a,b){
+       return b.price - a.price;
+    }
+
+    function sortByNameASC(a,b){
+      return a.name == b.name ? 0 : a.name < b.name ? -1 : 1;
+    }
+    function sortByNameDESC(a,b){
+      return a.name == b.name ? 0 : b.name < a.name ? -1 : 1;
+    }
+
+    function orderProducts(category, sorting){
+      var sortedProducts = [];
+      var filtredProducts = items.filter(function(i){
+        if(category == "all"){
+          return true;
+        }else{
+          return (i.category == category);
+        }   
+      });
+
+     console.log(sorting);
+     if(sorting === "price-up")
+       sortedProducts = filtredProducts.sort(sortByPriceASC);
+     if(sorting === "price-down")
+       sortedProducts = filtredProducts.sort(sortByPriceDESC);
+     if(sorting === "alpha-up")
+       sortedProducts = filtredProducts.sort(sortByNameASC);
+     if(sorting === "alpha-down")
+       sortedProducts = filtredProducts.sort(sortByNameDESC);
+
+
+     console.log(sortedProducts);
+     return sortedProducts;
+
+    }
+
+    function updateNbrProducts(products){
+      $("#products-count").html(products.length);
     }
 
     $("#product-categories > button").click((evt) => {
@@ -41,8 +87,20 @@
           return (i.category == selectedCategory);
         }   
       });
+
+      updateNbrProducts(filtredProducts);
+     
       listProduct(filtredProducts);
     });
+
+    $("#product-criteria > button").click((evt) => {
+      $(event.target).addClass("selected").siblings().removeClass('selected');
+      var selectedSorting = event.target.getAttribute('name');
+      var selectedCategory = $( "#product-categories .selected" ).attr("name");
+      var sortedProducts = orderProducts(selectedCategory,selectedSorting);
+      listProduct(sortedProducts);
+    });
+
 
     $("#products-list a>img").click((evt) => {
       let a =$(evt.target);
