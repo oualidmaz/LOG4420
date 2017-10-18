240b8ee51ccdeb1f82e6d006b6c8f07d79c2fd1e
oualidmaz <oualid.mazouzi@polymtl.ca>
2017-10-17 22:22:07 -0400

    draft code

--- TP3/assets/scripts/controllers/product-controller.js
+++ TP3/assets/scripts/controllers/product-controller.js
@@ -8,6 +8,14 @@
       Tp3.service.shoppingCart.add(input.data("product-id"), input.val());
       return false;
     });
+    // hide shopping cart if there is no item on it
+    var nbrItemOnShoppingCart = JSON.parse(localStorage.getItem("shopping-cart")).length;
+    $('.shopping-cart .count').html(nbrItemOnShoppingCart);
+    if(nbrItemOnShoppingCart === 0){
+      $('.shopping-cart .count').hide();
+    }else{
+      $('.shopping-cart .count').show();
+    }
   });
 
 })();
--- TP3/assets/scripts/controllers/products-controller.js
+++ TP3/assets/scripts/controllers/products-controller.js
@@ -7,7 +7,43 @@
     var items =[];
     Tp3.service.getItemIds().then((data)=>{
       items = data;
+      items.sort(function (a,b){
+        if (a.price < b.price)
+           return -1;
+        if (a.price> b.price)
+           return 1;
+        return 0;
     });
+
+      listProduct(items);
+    });
+
+    function listProduct(products){
+      $('#products-list').html('');
+      $.each(products, function(index, item) {
+        $('#products-list').append(`
+        <a href="./product.html" title="En savoir plus...">
+          <h2>${item.name}</h2>
+          <img alt="${item.name}" src="./assets/img/${item.image}">
+          <p><small>Prix</small> ${item.price}&thinsp;$</p>
+        </a>
+        `);
+     });
+    }
+
+    $("#product-categories > button").click((evt) => {
+      $(event.target).addClass("selected").siblings().removeClass('selected');
+      var selectedCategory = event.target.getAttribute('name');
+      var filtredProducts = items.filter(function(i){
+        if(selectedCategory == "all"){
+          return true;
+        }else{
+          return (i.category == selectedCategory);
+        }   
+      });
+      listProduct(filtredProducts);
+    });
+
     $("#products-list a>img").click((evt) => {
       let a =$(evt.target);
       let h2 =$(a).prev("h2");
