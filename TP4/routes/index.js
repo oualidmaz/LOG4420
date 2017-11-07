f39ac6e86fc96ec4a7d0242a33ac71d887139536
oualidmaz <oualid.mazouzi@polymtl.ca>
2017-11-06 22:07:56 -0500

    layout pug

--- TP4/routes/index.js
+++ TP4/routes/index.js
@@ -3,22 +3,25 @@ var router = express.Router();
 
 
 router.get("/produit", function(req, res) {
-  res.render("produit", { title: "produit", message: "page produit" });
+  res.render("produit", { title: "Produit", message: "page produit" });
 });
 router.get("/produits", function(req, res) {
-  res.render("produits", { title: "produits", message: "page produits" });
+  res.render("produits", { title: "Produits", message: "page produits" });
 });
 router.get("/contact", function(req, res) {
-  res.render("contact", { title: "contact", message: "page contact" });
+  res.render("contact", { title: "Contact", message: "page contact" });
 });
 router.get("/panier", function(req, res) {
-  res.render("panier", { title: "panier", message: "page panier" });
+  res.render("panier", { title: "Panier", message: "page panier" });
 });
 router.get("/commande", function(req, res) {
-  res.render("commande", { title: "commande", message: "page commande" });
+  res.render("commande", { title: "Commande", message: "page commande" });
 });
-router.get("/confirmation", function(req, res) {
-  res.render("confirmation", { title: "confirmation", message: "page confirmation" });
+router.get("/produit", function(req, res) {
+  res.render("produit", { title: "Produit", message: "page produit" });
+});
+router.get("/shopping-cart", function(req, res) {
+  res.render("shopping-cart", { title: "Shopping-cart", message: "page shopping cart" });
 });
 router.get("/", function(req, res) {
   res.render("index", { title: "Accueil", message: "Ça semble fonctionner!" });
